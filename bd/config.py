e73a8bde1f3539d7de03795f8a99040ccf9c21dd
Joseph Romero <88736577+josephypr@users.noreply.github.com>
2024-09-28 23:44:14 -0500

    Delete bd directory

--- bd/config.py
+++
@@ -1,8 +0,0 @@
-class Config:
-    DEBUG = True
-    SECRET_KEY = 'clave'
-    MYSQL_HOST = 'localhost'
-    MYSQL_USER = 'root'
-    MYSQL_PASSWORD = '1234'
-    MYSQL_DB = 'spr'
-    SESSION_COOKIE_SECURE = False
